2d511011fa2be725a4532549a0c8d374e0562376
Ryan Scott Keisler <rkeisler@corn16.stanford.edu>
2013-12-13 17:57:27 -0800

    minmax issues

--- sat.py
+++ sat.py
@@ -19,24 +19,28 @@ def latlong_to_ZXY_string(lat_deg, lon_deg, zoom):
     x,y,z = latlong_to_xyz(lat_deg, lon_deg, zoom)
     return xyz_to_ZXY_string(x,y,z)
 
-def latlong_rectange_to_xyz(lat_min, lat_max, lon_min, lon_max, zoom):
+def latlong_rectange_to_xyz(lat1, lat2, lon1, lon2, zoom):
+    lat_min=np.min([lat1,lat2])
+    lat_max=np.max([lat1,lat2])    
+    lon_min=np.min([lon1,lon2])
+    lon_max=np.max([lon1,lon2])        
     x_min, y_max, zoom = latlong_to_xyz(lat_min, lon_min, zoom)
     x_max, y_min, zoom = latlong_to_xyz(lat_max, lon_max, zoom)
     return x_min, x_max, y_min, y_max
 
 def xyz_to_savename(x,y,z,prefix='tmp'):
-    return prefix+'_%i_%i_%i'%(x,y,z)+'.png'
+    return prefix+'_x%i_y%i_z%i'%(x,y,z)+'.png'
 
 def download_one(x,y,zoom,prefix='tmp'):
     url=http_base+xyz_to_ZXY_string(x,y,zoom)+'.png'
     savename=savepath+xyz_to_savename(x,y,zoom,prefix=prefix)
     urlretrieve(url, savename)
     
-def download_rectangle(lat_min, lat_max, lon_min, lon_max, 
+def download_rectangle(lat1, lat2, lon1, lon2, 
                        zoom, prefix='tmp', download=True):
-    x_min, x_max, y_min, y_max = latlong_rectange_to_xyz(lat_min, lat_max, lon_min, lon_max, zoom)
-    n_x = np.abs(x_max-x_min+1)
-    n_y = np.abs(y_max-y_min+1)
+    x_min, x_max, y_min, y_max = latlong_rectange_to_xyz(lat1, lat2, lon1, lon2, zoom)
+    n_x = x_max-x_min+1
+    n_y = y_max-y_min+1
     n_tiles = n_x*n_y
     x_count=0
     print 'Downloading X=(%i,%i), Y=(%i,%i)'%(x_min,x_max,y_min,y_max)
@@ -50,18 +54,18 @@ def download_rectangle(lat_min, lat_max, lon_min, lon_max,
 
 def define_chunks():
     sf1=dict(prefix='sf1',
-             lat_min=30.0, lax_max=30.1, 
-             lon_min=-50.0, lon_max=-49.9)
+             lat1=30.0, lax_max=30.1, 
+             lon1=-50.0, lon2=-49.9)
 
     chi=dict(prefix='chi', 
-             lat_min=hms_to_deg(41,46,24.77),
-             lat_max=hms_to_deg(41,59,51.98),
-             long_min=hms_to_deg(-87.728894), 
-             long_max=hms_to_deg(-87.564863))
+             lat1=hms_to_deg(41,46,24.77),
+             lat2=hms_to_deg(41,59,51.98),
+             lon1=hms_to_deg(-87,43,44.02),
+             lon2=hms_to_deg(-87,33,53.51))
 
     atx=dict(prefix='atx')
     
-    chunks=dict(sf1=sf1)
+    chunks=dict(sf1=sf1, chi=chi)
     return chunks
 
 def hms_to_deg(hour, min, sec):
